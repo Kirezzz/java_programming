90980b478daaeb3d36576d785f0856d43e4b28fd
Kirill Nikitin <kirez777@gmail.com>
2016-09-13 17:21:26 +0300

    Создал классы Square и Rectangle. Вынес методы в эти классы

---
+++ sandbox/src/main/java/ru/stqa/java/sandbox/Square.java
@@ -0,0 +1,16 @@
+package ru.stqa.java.sandbox;
+
+/**
+ * Created by Sony on 13.09.2016.
+ */
+public class Square {
+  public double l;
+
+  public Square(double l) {
+    this.l = l;
+  }
+
+  public double area() {
+    return this.l * this.l;
+  }
+}
