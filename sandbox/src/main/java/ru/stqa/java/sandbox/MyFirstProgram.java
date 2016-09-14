96a2c54d9a145cf27f96cdf3e9154281c5bc39de
Kirill Nikitin <kirez777@gmail.com>
2016-09-14 17:07:37 +0300

    Вынес метод подсчета расстоания между двумя точками в класс Point

--- sandbox/src/main/java/ru/stqa/java/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/java/sandbox/MyFirstProgram.java
@@ -15,14 +15,9 @@ public class MyFirstProgram {
         Rectangle r = new Rectangle(4, 5);
         System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " равна " + r.area());
 
-        Point p1 = new Point();
-        Point p2 = new Point();
-        p1.x = 3;
-        p1.y = 2;
-        p2.x = 4;
-        p2.y = 6;
+        Point p1 = new Point(1, 2);
+        Point p2 = new Point(3, 4);
 
-        distance(p1, p2);
 
     }
 
@@ -30,9 +25,5 @@ public class MyFirstProgram {
         System.out.println("Hello, " + somebody);
     }
 
-    public static void distance(Point p1, Point p2) {
-        System.out.println(Math.sqrt(Math.pow(p2.x - p1.x, 2) + Math.pow(p2.y - p1.y, 2)));
-
-    }
 
 }
--- sandbox/src/main/java/ru/stqa/java/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/java/sandbox/Point.java
@@ -7,4 +7,13 @@ public class Point {
   public double x;
   public double y;
 
+
+  public Point(double p1, double p2) {
+    this.x = p1;
+    this.y = p2;
+  }
+
+  public static double distance(Point p1, Point p2) {
+    return Math.sqrt(Math.pow(p2.x - p1.x, 2) + Math.pow(p2.y - p1.y, 2));
+  }
 }
