d6ce9ba0b7bb86a9debff0ccc6826796767b2e63
Kirill Nikitin <kirez777@gmail.com>
2016-09-26 22:50:50 +0300

    Свернутая форма else if

--- sandbox/src/main/java/ru/stqa/java/sandbox/Equation.java
+++ sandbox/src/main/java/ru/stqa/java/sandbox/Equation.java
@@ -19,15 +19,12 @@ public class Equation {
 
     if (d > 0) {
       n = 2;
+    } else if (d == 0) {
+      n = 1;
+    } else {
+      n = 0;
     }
-    else {
-      if (d == 0) {
-        n = 1;
-      }
-      else {
-        n = 0;
-      }
-    }
+
   }
   public int rootNumber() {
     return n;
