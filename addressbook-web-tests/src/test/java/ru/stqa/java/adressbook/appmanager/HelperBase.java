58f84049cfa72d9b3f908ed670d0f4792da2907e
Kirill Nikitin <kirez777@gmail.com>
2016-09-28 22:42:41 +0300

    Реализовал ленивое заполнение полей

--- addressbook-web-tests/src/test/java/ru/stqa/java/adressbook/appmanager/HelperBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/java/adressbook/appmanager/HelperBase.java
@@ -3,7 +3,6 @@ package ru.stqa.java.adressbook.appmanager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 
 /**
  * Created by Sony on 22.09.2016.
@@ -22,10 +21,14 @@ public class HelperBase {
   protected void type(By locator, String text) {
     click(locator);
     if (text != null) {
-      wd.findElement(locator).clear();
-      wd.findElement(locator).sendKeys(text);
+      String existingText = wd.findElement(locator).getAttribute("value");
+      if (! text.equals(existingText)) {
+        wd.findElement(locator).clear();
+        wd.findElement(locator).sendKeys(text);
+      }
     }
   }
+
   public boolean isAlertPresent() {
     try {
       wd.switchTo().alert();
@@ -34,6 +37,7 @@ public class HelperBase {
       return false;
     }
   }
+
   public void acceptAlert() {
     wd.switchTo().alert().accept();
   }
