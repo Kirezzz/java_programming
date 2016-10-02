a810ebae719bdf6fc60ec96ee05b81920eef2df1
Kirill Nikitin <kirez777@gmail.com>
2016-10-02 23:25:31 +0300

    Оптимизировал количество выполнений до n / 2 итераций

--- sandbox/src/test/java/ru/stqa/java/sandbox/PrimeTests.java
+++ sandbox/src/test/java/ru/stqa/java/sandbox/PrimeTests.java
@@ -9,7 +9,7 @@ import org.testng.annotations.Test;
 public class PrimeTests {
   @Test
   public void testPrime() {
-    Assert.assertTrue(Primes.isPrime(Integer.MAX_VALUE));
+    Assert.assertTrue(Primes.isPrimeFast(Integer.MAX_VALUE));
   }
 
   @Test(enabled = false)
