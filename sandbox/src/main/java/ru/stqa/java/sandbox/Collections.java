35a25a85302b1ab108454d9aeda04810bf0b97a9
Kirill Nikitin <kirez777@gmail.com>
2016-10-03 21:49:45 +0300

    Реализован цикл для работы с массивом

--- sandbox/src/main/java/ru/stqa/java/sandbox/Collections.java
+++ sandbox/src/main/java/ru/stqa/java/sandbox/Collections.java
@@ -6,5 +6,8 @@ package ru.stqa.java.sandbox;
 public class Collections {
   public static void main(String args[]) {
     String[] langs = {"Java","C#","Python", "PHP"};
+    for (int i = 0; i < langs.length; i++) {
+      System.out.println("Я хочу выучить " + langs[i]);
+    }
   }
 }
