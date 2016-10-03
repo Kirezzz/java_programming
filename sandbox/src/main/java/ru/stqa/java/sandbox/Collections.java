f7e2d4d9d372dc24b2d7a57b369bac551e2925dd
Kirill Nikitin <kirez777@gmail.com>
2016-10-03 21:41:48 +0300

    Массив наглядная версия

---
+++ sandbox/src/main/java/ru/stqa/java/sandbox/Collections.java
@@ -0,0 +1,14 @@
+package ru.stqa.java.sandbox;
+
+/**
+ * Created by Sony on 03.10.2016.
+ */
+public class Collections {
+  public static void main(String args[]) {
+    String[] langs = new String[4];
+    langs[0] = "Java";
+    langs[1] = "C#";
+    langs[2] = "Python";
+    langs[3] = "PHP";
+  }
+}
