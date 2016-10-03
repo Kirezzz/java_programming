e11fce81888ea1305a7b33010f5b95b71f33bfb9
Kirill Nikitin <kirez777@gmail.com>
2016-10-03 22:00:41 +0300

    Реализация списка

--- sandbox/src/main/java/ru/stqa/java/sandbox/Collections.java
+++ sandbox/src/main/java/ru/stqa/java/sandbox/Collections.java
@@ -1,10 +1,21 @@
 package ru.stqa.java.sandbox;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
 /**
  * Created by Sony on 03.10.2016.
  */
 public class Collections {
   public static void main(String args[]) {
+
+    List<String> languages = new ArrayList<String>();
+    languages.add("Java");
+    languages.add("C#");
+    languages.add("Python");
+    languages.add("PHP");
+
     String[] langs = {"Java","C#","Python", "PHP"};
     for (String l : langs) {
       System.out.println("Я хочу выучить " + l);
