f7b4e66c51eacc4ffeec68e66080fcf2760ddf32
Kirill Nikitin <kirez777@gmail.com>
2016-10-04 17:38:38 +0300

    Добавил создание коллекций в тестах для Групп

--- addressbook-web-tests/src/test/java/ru/stqa/java/adressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/java/adressbook/appmanager/GroupHelper.java
@@ -2,9 +2,13 @@ package ru.stqa.java.adressbook.appmanager;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import ru.stqa.java.adressbook.model.GroupData;
 import ru.stqa.java.adressbook.tests.TestBase;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Sony on 22.09.2016.
@@ -64,4 +68,14 @@ public class GroupHelper extends HelperBase{
     return  wd.findElements(By.name("selected[]")).size();
   }
 
+  public List<GroupData> getGroupList() {
+    List<GroupData> groups = new ArrayList<GroupData>();
+    List<WebElement> elements = wd.findElements(By.cssSelector("span.group"));
+    for (WebElement element : elements) {
+      String name = element.getText();
+      GroupData group = new GroupData(name, null, null);
+      groups.add(group);
+    }
+    return groups;
+  }
 }
