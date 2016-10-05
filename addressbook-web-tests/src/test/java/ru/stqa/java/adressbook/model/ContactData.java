6a8fd9e830ec96d9a53c690073bbc0c29b7c9598
Kirill Nikitin <kirez777@gmail.com>
2016-10-06 00:03:35 +0300

    Добавил построчное сравнение списков при удалении контактов

--- addressbook-web-tests/src/test/java/ru/stqa/java/adressbook/model/ContactData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/java/adressbook/model/ContactData.java
@@ -46,4 +46,43 @@ public class ContactData {
   public String getGroup() {
     return group;
   }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+
+    ContactData that = (ContactData) o;
+
+    if (firstname != null ? !firstname.equals(that.firstname) : that.firstname != null) return false;
+    if (lastname != null ? !lastname.equals(that.lastname) : that.lastname != null) return false;
+    if (address1 != null ? !address1.equals(that.address1) : that.address1 != null) return false;
+    if (telhome != null ? !telhome.equals(that.telhome) : that.telhome != null) return false;
+    return email1 != null ? email1.equals(that.email1) : that.email1 == null;
+
+  }
+
+  @Override
+  public int hashCode() {
+    int result = firstname != null ? firstname.hashCode() : 0;
+    result = 31 * result + (lastname != null ? lastname.hashCode() : 0);
+    result = 31 * result + (address1 != null ? address1.hashCode() : 0);
+    result = 31 * result + (telhome != null ? telhome.hashCode() : 0);
+    result = 31 * result + (email1 != null ? email1.hashCode() : 0);
+    return result;
+  }
+
+  @Override
+  public String toString() {
+    return "ContactData{" +
+            "firstname='" + firstname + '\'' +
+            ", middlename='" + middlename + '\'' +
+            ", lastname='" + lastname + '\'' +
+            ", address1='" + address1 + '\'' +
+            ", telhome='" + telhome + '\'' +
+            ", email1='" + email1 + '\'' +
+            ", group='" + group + '\'' +
+            '}';
+
+  }
 }
