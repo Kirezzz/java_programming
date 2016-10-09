72264a552831d0e8cf3b5e6c33ac217e9c0604f1
Kirill Nikitin <kirez777@gmail.com>
2016-10-09 22:08:08 +0300

    Оптимизировал тест модификации контактов

--- addressbook-web-tests/src/test/java/ru/stqa/java/adressbook/tests/ContactModificationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/java/adressbook/tests/ContactModificationTest.java
@@ -1,6 +1,7 @@
 package ru.stqa.java.adressbook.tests;
 
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.stqa.java.adressbook.model.ContactData;
 import ru.stqa.java.adressbook.model.GroupData;
@@ -13,25 +14,31 @@ import java.util.List;
  * Created by Sony on 24.09.2016.
  */
 public class ContactModificationTest extends TestBase {
-  @Test(enabled = false)
-  public void testContactModification() {
+  @BeforeMethod
+  public void ensurePreconditions() {
     app.getNavigationHelper().gotoHomePage();
     //Если контактов нет, то создается контакт
+    ContactData contact = new ContactData(
+            "First name1",
+            "Middle name1",
+            "Last name1",
+            "address1",
+            "111-222-333",
+            "test1@test.com",
+            "test1")
     if (! app.getContactHelper().isThereAContact()) {
-      app.getContactHelper().createContact(new ContactData(
-              "First name1",
-              "Middle name1",
-              "Last name1",
-              "address1",
-              "111-222-333",
-              "test1@test.com",
-              "test1"), true);
+      app.getContactHelper().createContact(contact, true);
     }
 
+  }
+
+  @Test(enabled = false)
+  public void testContactModification() {
+
     List<ContactData> before = app.getContactHelper().getContactList();
-    app.getContactHelper().initContactModification(before.size() - 1);
+    int index = before.size() - 1;
     ContactData contact = new ContactData(
-            before.get(before.size() - 1).getId(),
+            before.get(index).getId(),
             "First name1",
             "Middle name1",
             "Last name1",
@@ -39,13 +46,14 @@ public class ContactModificationTest extends TestBase {
             "111-000-333",
             "test2@test.com",
             null);
+    app.getContactHelper().initContactModification(index);
     app.getContactHelper().fillContactForm(contact, false);
     app.getContactHelper().submitContactModification();
     app.getContactHelper().returnToHomePage();
     List<ContactData> after = app.getContactHelper().getContactList();
     Assert.assertEquals(after.size(), before.size());
 
-    before.remove(before.size() - 1);
+    before.remove(index);
     before.add(contact);
     Comparator<? super ContactData> byId = (c1, c2) -> Integer.compare(c1.getId(), c2.getId());
     before.sort(byId);
