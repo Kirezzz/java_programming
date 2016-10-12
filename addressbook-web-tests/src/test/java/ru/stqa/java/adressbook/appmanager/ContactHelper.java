3d0d906d6e64f44e0b9fab8c5c9cdf24137e518e
Kirill Nikitin <kirez777@gmail.com>
2016-10-12 12:28:31 +0300

    Убрал лишнюю скобку в локаторе

--- addressbook-web-tests/src/test/java/ru/stqa/java/adressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/java/adressbook/appmanager/ContactHelper.java
@@ -86,7 +86,7 @@ public class ContactHelper extends HelperBase {
   }
 
   public void initContactModificationById(int id) {
-    wd.findElement((By.cssSelector("a[href]=edit.php?id='" + id + "']"))).click();
+    wd.findElement((By.cssSelector("a[href=edit.php?id='" + id + "']"))).click();
   }
 
   public void submitContactModification() {
