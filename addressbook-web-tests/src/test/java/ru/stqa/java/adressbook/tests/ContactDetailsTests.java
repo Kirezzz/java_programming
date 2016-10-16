e80b89a522aea1045ffcee4ad199e47c341e9eae
Kirill Nikitin <kirez777@gmail.com>
2016-10-16 22:27:35 +0300

    Небольшие доработки

--- addressbook-web-tests/src/test/java/ru/stqa/java/adressbook/tests/ContactDetailsTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/java/adressbook/tests/ContactDetailsTests.java
@@ -18,18 +18,16 @@ public class ContactDetailsTests extends TestBase {
   @Test
   public void testContactDetails() {
     app.goTo().homePage();
-    ContactData contact = app.contact().all().iterator().next();
-
+    ContactData contact = app.contact().allForDetails().iterator().next();
     ContactData contactInfoDetailsForm = app.contact().contactInfoDetailsForm(contact);
-    assertThat(mergeAllContactInfo(contact.getAllContactinfo()), equalTo(mergeAllContactInfo(contactInfoDetailsForm)));
-
+    assertThat(contact.getAllContactinfo(), equalTo(mergeAllContactInfo(contactInfoDetailsForm)));
   }
 
   private String mergeAllContactInfo(ContactData contact) {
     return Arrays.asList(contact.getLastname(),contact.getFirstname(),contact.getAddress(),
             contact.getEmail(),contact.getEmail2(),contact.getEmail3(),
             contact.getHomePhone(),contact.getMobilePhone(),contact.getWorkPhone())
-            .stream().filter((s) -> ! s.equals(""))
+            .stream().filter((s) -> s!=null && !s.equals(""))
             .map(ContactDetailsTests::cleaned)
             .collect(Collectors.joining("\n"));
 
@@ -37,9 +35,6 @@ public class ContactDetailsTests extends TestBase {
 
   public static String cleaned(String details) {
     return details.replaceAll("\\s", "")
-            .replaceAll("H:", "")
-            .replaceAll("W:", "")
-            .replaceAll("M:", "")
             .replaceAll("[-()]", "");
   }
 
