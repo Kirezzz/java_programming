15839547605b5dabdca9ce2b6b4c988a0b65edff
Kirill Nikitin <kirez777@gmail.com>
2016-10-18 23:18:21 +0300

    Поправил ошибку в генераторе

--- addressbook-web-tests/src/test/java/ru/stqa/java/adressbook/generators/GroupDataGenerator.java
+++ addressbook-web-tests/src/test/java/ru/stqa/java/adressbook/generators/GroupDataGenerator.java
@@ -34,12 +34,12 @@ public class GroupDataGenerator {
 
   private static List<GroupData> generateGroups(int count) {
     List<GroupData> groups = new ArrayList<GroupData>();
-    for(int i = 0; i < count; i++) {
-    groups.add(new GroupData()
-            .withName(String.format("test %s", i))
-            .withHeader(String.format("header %s", i))
-            .withFooter(String.format("footer %s", i)));
+    for (int i = 0; i < count; i++) {
+      groups.add(new GroupData()
+              .withName(String.format("test %s", i))
+              .withHeader(String.format("header %s", i))
+              .withFooter(String.format("footer %s", i)));
     }
-      return null;
+    return groups;
     }
-}
+  }
